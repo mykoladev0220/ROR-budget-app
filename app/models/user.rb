9726d9cdcc38691ca3f530f065c322c8ba120bf5
mykoladev0220 <mykolademchukdev@gmail.com>
2023-08-16 14:59:44 +0000

    Create user and group models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User<ApplicationRecord
+    attr_accessor :full_name
     has_many :groups
-    has_many :expenses 
+    has_many :expenses
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable
+
+    validates :full_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length:{minimum: 6}
 end
