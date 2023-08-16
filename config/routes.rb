b8f9abfa3fba680ffc301297555dd54302dcbc7d
mykoladev0220 <mykolademchukdev@gmail.com>
2023-08-16 14:58:38 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "groups#index"
+  resources :users, only: [:index, :show] do
+    resources :groups, only: [:index, :new, :create, :destroy] do
+      resources :expenses, only: [:index, :create, :new] 
+    end
+  end
 end
