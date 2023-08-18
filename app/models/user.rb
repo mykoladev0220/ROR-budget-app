acbabcdfef50f8286f2ca6d7e9936f2bd4fd5c44
mykoladev0220 <mykolademchukdev@gmail.com>
2023-08-18 01:15:50 +0000

    Update groups and users models

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :full_name, presence: true
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
