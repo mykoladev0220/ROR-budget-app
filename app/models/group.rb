fff195a755a29aee05e4b90e087c1f504285d996
mykoladev0220 <mykolademchukdev@gmail.com>
2023-08-19 09:05:22 +0000

    Corrrect rubocop linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,6 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :expenses, join_table: 'groups_expenses'
 
-
   validates :name, presence: true
   validates :icon, presence: true
 
